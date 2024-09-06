839c589dadb78f060b1af48edc3e331a9c4b73cd
Alexandre31470 <alexandre.quev@gmail.com>
2024-09-06 11:21:35 +0200

    commit modif stucture suite erreurs

--- views/common/header.php
+++ views/common/header.php
@@ -1,13 +1,10 @@
-
-
 <div class="container">
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        <img src="<?= URL; ?>public/Assets/images/logo.png" width="40" alt="logo du site">
-        <h1>Mon site Web en MVC</h1>
-      </a>
+  <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+      <img src="<?= URL; ?>public/Assets/images/logo.png" width="40" alt="logo du site" />
+      <span class="ms-5 fs-4">Mon super site</span>
+    </a>
 
-      <?php require_once("views/common/menu.php"); ?> <!--Affichage de la navbar depuis menu.php-->
-    </header>
-  </div>
-
+    <?php require_once("views/common/menu.php"); ?>
+  </header>
+</div>
--- views/common/template.php
+++ views/common/template.php
@@ -1,38 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <meta name="description" content="<?= $page_description ?>"> <!--Ici $page_description permet modification dynamique depuis le MainController.controller.php -->
-    <title><?= $page_title ?></title> <!--Ici $page_title permet modification dynamique depuis le MainController.controller.php  -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link  href="<?= URL ?>public/CSS/main.css"  rel="stylesheet">
-    <?php if(!empty($page_css) && is_array($page_css)) : ?>
-    <?php foreach($page_css as $fichier_css) : ?>
-    <link  href="<?= URL ?>public/CSS/<?= $fichier_css ?>" rel="stylesheet">
-    <?php endforeach ?>
+    <meta name="description" content="<?= $page_description; ?>">
+    <title><?= $page_title; ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link href="<?= URL ?>public/CSS/main.css" rel="stylesheet" />
+    <?php if(!empty($page_css)) : ?>
+        <?php foreach($page_css as $fichier_css) : ?>
+            <link href="<?= URL ?>public/CSS/<?= $fichier_css ?>" rel="stylesheet" />
+        <?php endforeach; ?>
     <?php endif; ?>
-
 </head>
 <body>
+    <?php require_once("views/common/header.php"); ?>
 
- <?php require_once("views/common/header.php"); ?> <!--Affichage du fichier header.php-->
-
- <div class="container">
-    <?php if(!empty($_SESSION['alert'])) : ?> <!--Vérifie si la variable session est vide -->
-    <div class="alert <?=$_SESSION['alert']['type']; ?>" role="alert"> <!--Ajoute dynamiquement une autre classe provenant du type d'alerte stocké dans $_SESSION-->
-        <?=$_SESSION['alert']['message']; ?> <!-- Affiche le message de l'alerte, qui est stocké dans $_SESSION['alert']['message'] -->
+    <div class="container">
+        <?php 
+            if(!empty($_SESSION['alert'])) {
+                foreach($_SESSION['alert'] as $alert){
+                    echo "<div class='alert ". $alert['type'] ."' role='alert'>
+                        ".$alert['message']."
+                    </div>";
+                }
+                unset($_SESSION['alert']);
+            }
+        ?>
+        <?= $page_content; ?>
     </div>
-<?php 
-unset($_SESSION['alert']);//Supprime la variable de session $_SESSION['alert'] après l'avoir affichée
-endif; ?>
 
-<?= $page_content ?></div><!--Ici $page_description permet modification dynamique depuis le MainController.controller.php-->
+    <?php require_once("views/common/footer.php"); ?>
 
-    <?php require_once("views/common/footer.php"); ?> <!--Affichage du fichier footer.php-->
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <?php if(!empty($page_javascript)) : ?>
+        <?php foreach($page_javascript as $fichier_javascript) : ?>
+            <script src="<?= URL?>public/JavaScript/<?= $fichier_javascript ?>"></script>
+        <?php endforeach; ?>
+    <?php endif; ?>
 </body>
-
 </html>
